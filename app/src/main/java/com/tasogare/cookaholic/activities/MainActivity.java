a2abd5833564589b5ed76c39c0cbddb5c3619f6d
kradragon <tuanna.hsp@gmail.com>
2017-03-10 18:02:54 +0700

    Create project

---
+++ app/src/main/java/com/tasogare/cookaholic/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tasogare.cookaholic.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tasogare.cookaholic.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
